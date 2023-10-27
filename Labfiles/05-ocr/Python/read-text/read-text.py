4d9c8b40a48f73f7f3d2a19cfe3855cae9270370
MScalopez <87540998+MScalopez@users.noreply.github.com>
2023-10-27 04:41:58 -0500

    Update read-text.py

--- Labfiles/05-ocr/Python/read-text/read-text.py
+++ Labfiles/05-ocr/Python/read-text/read-text.py
@@ -23,7 +23,7 @@ def main():
 
 
         # Menu for text reading functions
-        print('1: Use Read API for image (Lincoln.jpg)\n2: Read handwriting (Note.jpg)\nAny other key to quit')
+        print('\n1: Use Read API for image (Lincoln.jpg)\n2: Read handwriting (Note.jpg)\nAny other key to quit\n')
         command = input('Enter a number:')
         if command == '1':
             image_file = os.path.join('images','Lincoln.jpg')
