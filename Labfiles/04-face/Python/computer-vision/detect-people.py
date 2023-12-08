155ffc994a1f4f113c8f740907d1355eef218a42
Carlos Lopez <MScalopez@users.noreply.github.com>
2023-12-08 15:10:09 -0600

    Updating SDKs

---
+++ Labfiles/04-face/Python/computer-vision/detect-people.py
@@ -0,0 +1,48 @@
+from dotenv import load_dotenv
+import os
+from array import array
+from PIL import Image, ImageDraw
+import sys
+import time
+from matplotlib import pyplot as plt
+import numpy as np
+
+# Import namespaces
+
+
+
+def main():
+    global cv_client
+
+    try:
+        # Get Configuration Settings
+        load_dotenv()
+        ai_endpoint = os.getenv('AI_SERVICE_ENDPOINT')
+        ai_key = os.getenv('AI_SERVICE_KEY')
+
+        # Get image
+        image_file = 'images/people.jpg'
+        if len(sys.argv) > 1:
+            image_file = sys.argv[1]
+
+        # Authenticate Azure AI Vision client
+
+        
+        # Analyze image
+        AnalyzeImage(image_file, cv_client)
+
+    except Exception as ex:
+        print(ex)
+
+
+def AnalyzeImage(image_file, cv_client):
+    print('\nAnalyzing', image_file)
+
+    # Specify features to be retrieved
+
+
+    # Get image analysis
+
+
+if __name__ == "__main__":
+    main()
