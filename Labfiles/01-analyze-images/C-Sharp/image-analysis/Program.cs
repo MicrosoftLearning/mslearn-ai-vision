d6bd7caf1b11b3d513c593d587576398091dc05e
Ivor Berry <ivorb@users.noreply.github.com>
2024-02-13 09:04:53 -0500

    updated Vision versions

--- Labfiles/01-analyze-images/C-Sharp/image-analysis/Program.cs
+++ Labfiles/01-analyze-images/C-Sharp/image-analysis/Program.cs
@@ -1,18 +1,23 @@
 using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 using System.Drawing;
 using Microsoft.Extensions.Configuration;
 using Azure;
-using System.IO;
 
 // Import namespaces
-
+using Azure.AI.Vision.ImageAnalysis;
 
 namespace image_analysis
 {
     class Program
     {
 
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             try
             {
@@ -30,13 +35,16 @@ namespace image_analysis
                 }
 
                 // Authenticate Azure AI Vision client
-
+                // Authenticate Azure AI Vision client
+                ImageAnalysisClient client = new ImageAnalysisClient(
+                    new Uri(aiSvcEndpoint),
+                    new AzureKeyCredential(aiSvcKey));
                 
                 // Analyze image
-                AnalyzeImage(imageFile, cvClient);
+                AnalyzeImage(imageFile, client);
 
                 // Remove the background or generate a foreground matte from the image
-                BackgroundForeground(imageFile, cvClient);
+                await BackgroundForeground(imageFile, aiSvcEndpoint, aiSvcKey);
 
             }
             catch (Exception ex)
@@ -45,23 +53,147 @@ namespace image_analysis
             }
         }
 
-        static void AnalyzeImage(string imageFile, VisionServiceOptions serviceOptions)
+        static void AnalyzeImage(string imageFile, ImageAnalysisClient client)
         {
             Console.WriteLine($"\nAnalyzing {imageFile} \n");
 
-            var analysisOptions = new ImageAnalysisOptions()
+            // Use a file stream to pass the image data to the analyze call
+            using FileStream stream = new FileStream(imageFile,
+                                                     FileMode.Open);
+
+            // Get result with specified features to be retrieved
+            ImageAnalysisResult result = client.Analyze(
+                BinaryData.FromStream(stream),
+                VisualFeatures.Caption | 
+                VisualFeatures.DenseCaptions |
+                VisualFeatures.Objects |
+                VisualFeatures.Tags |
+                VisualFeatures.People);
+            
+            // Get image captions
+            if (result.Caption.Text != null)
             {
-                // Specify features to be retrieved
+                Console.WriteLine(" Caption:");
+                Console.WriteLine($"   \"{result.Caption.Text}\", Confidence {result.Caption.Confidence:F2}\n");
+            }
 
-            };
+            // Get image dense captions
+            Console.WriteLine(" Dense Captions:");
+            foreach (DenseCaption denseCaption in result.DenseCaptions.Values)
+            {
+                Console.WriteLine($"   Caption: '{denseCaption.Text}', Confidence: {denseCaption.Confidence:F2}");
+            }            
 
-            // Get image analysis
+            // Get image tags
+            if (result.Tags.Values.Count > 0)
+            {
+                Console.WriteLine($" Tags:");
+                foreach (DetectedTag tag in result.Tags.Values)
+                {
+                    Console.WriteLine($"   '{tag.Name}', Confidence: {tag.Confidence:F2}");
+                }
+            }
+
+            // Get objects in the image
+            if (result.Objects.Values.Count > 0)
+            {
+                Console.WriteLine(" Objects:");
+
+                // Prepare image for drawing
+                stream.Close();
+                System.Drawing.Image image = System.Drawing.Image.FromFile(imageFile);
+                Graphics graphics = Graphics.FromImage(image);
+                Pen pen = new Pen(Color.Cyan, 3);
+                Font font = new Font("Arial", 16);
+                SolidBrush brush = new SolidBrush(Color.WhiteSmoke);
+
+                foreach (DetectedObject detectedObject in result.Objects.Values)
+                {
+                    Console.WriteLine($"   \"{detectedObject.Tags[0].Name}\"");
+
+                    // Draw object bounding box
+                    var r = detectedObject.BoundingBox;
+                    Rectangle rect = new Rectangle(r.X, r.Y, r.Width, r.Height);
+                    graphics.DrawRectangle(pen, rect);
+                    graphics.DrawString(detectedObject.Tags[0].Name,font,brush,(float)r.X, (float)r.Y);
+                }
+
+                // Save annotated image
+                String output_file = "objects.jpg";
+                image.Save(output_file);
+                Console.WriteLine("  Results saved in " + output_file + "\n");
+            }
+
+            // Get people in the image
+            if (result.People.Values.Count > 0)
+            {
+                Console.WriteLine($" People:");
+
+                // Prepare image for drawing
+                System.Drawing.Image image = System.Drawing.Image.FromFile(imageFile);
+                Graphics graphics = Graphics.FromImage(image);
+                Pen pen = new Pen(Color.Cyan, 3);
+                Font font = new Font("Arial", 16);
+                SolidBrush brush = new SolidBrush(Color.WhiteSmoke);
+
+                foreach (DetectedPerson person in result.People.Values)
+                {
+                    // Draw object bounding box
+                    var r = person.BoundingBox;
+                    Rectangle rect = new Rectangle(r.X, r.Y, r.Width, r.Height);
+                    graphics.DrawRectangle(pen, rect);
+                    
+                    // Return the confidence of the person detected
+                    //Console.WriteLine($"   Bounding box {person.BoundingBox.ToString()}, Confidence: {person.Confidence:F2}");
+                }
+
+                // Save annotated image
+                String output_file = "persons.jpg";
+                image.Save(output_file);
+                Console.WriteLine("  Results saved in " + output_file + "\n");
+            }
 
         }
-        static void BackgroundForeground(string imageFile, VisionServiceOptions serviceOptions)
+        static async Task BackgroundForeground(string imageFile, string endpoint, string key)
         {
+            Console.WriteLine($" Background removal:");
+            
+            // Define the API version and mode
+            string apiVersion = "2023-02-01-preview";
+            string mode = "backgroundRemoval"; // Can be "foregroundMatting" or "backgroundRemoval"
+            
             // Remove the background from the image or generate a foreground matte
+            string url = $"computervision/imageanalysis:segment?api-version={apiVersion}&mode={mode}";
 
+            // Make the REST call
+            using (var client = new HttpClient())
+            {
+                var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+                client.BaseAddress = new Uri(endpoint);
+                client.DefaultRequestHeaders.Accept.Add(contentType);
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", key);
+
+                // You can change the url to use other images in the images folder,
+                // such as "building.jpg" or "person.jpg" to see different results.
+                var data = new
+                {
+                    url="https://github.com/MicrosoftLearning/mslearn-ai-vision/blob/main/Labfiles/01-analyze-images/Python/image-analysis/images/street.jpg?raw=true"
+                };
+
+                var jsonData = JsonSerializer.Serialize(data);
+                var contentData = new StringContent(jsonData, Encoding.UTF8, contentType);
+                var response = await client.PostAsync(url, contentData);
+
+                if (response.IsSuccessStatusCode) {
+                    File.WriteAllBytes("backgroundForeground.png", response.Content.ReadAsByteArrayAsync().Result);
+                    Console.WriteLine("  Results saved in backgroundForeground.png\n");
+                }
+                else
+                {
+                    Console.WriteLine($"API error: {response.ReasonPhrase} - Check your body url, key, and endpoint.");
+                }
+            }
+            
         }
     }
 }
--- Labfiles/01-analyze-images/Python/image-analysis/image-analysis.py
+++ Labfiles/01-analyze-images/Python/image-analysis/image-analysis.py
@@ -1,15 +1,15 @@
 from dotenv import load_dotenv
 import os
-from array import array
 from PIL import Image, ImageDraw
 import sys
-import time
 from matplotlib import pyplot as plt
-import numpy as np
+from azure.core.exceptions import HttpResponseError
+import requests
 
 # Import namespaces
-
-
+from azure.ai.vision.imageanalysis import ImageAnalysisClient
+from azure.ai.vision.imageanalysis.models import VisualFeatures
+from azure.core.credentials import AzureKeyCredential
 
 def main():
     global cv_client
@@ -25,34 +25,146 @@ def main():
         if len(sys.argv) > 1:
             image_file = sys.argv[1]
 
-        # Authenticate Azure AI Vision client
+        with open(image_file, "rb") as f:
+            image_data = f.read()
 
+        # Authenticate Azure AI Vision client
+        cv_client = ImageAnalysisClient(
+            endpoint=ai_endpoint,
+            credential=AzureKeyCredential(ai_key)
+        )
         
         # Analyze image
-        AnalyzeImage(image_file, cv_client)
-
-        # Generate thumbnail
-        BackgroundForeground(image_file, cv_client)
+        AnalyzeImage(image_file, image_data, cv_client)
+        
+        # Background removal
+        # BackgroundForeground(ai_endpoint, ai_key)
 
     except Exception as ex:
         print(ex)
 
 
-def AnalyzeImage(image_file, cv_client):
-    print('\nAnalyzing', image_file)
+def AnalyzeImage(image_filename, image_data, cv_client):
+    print('\nAnalyzing image...')
 
-    # Specify features to be retrieved
+    try:
+        # Get result with specified features to be retrieved
+        result = cv_client.analyze(
+            image_data=image_data,
+            visual_features=[
+                VisualFeatures.CAPTION,
+                VisualFeatures.DENSE_CAPTIONS,
+                VisualFeatures.TAGS,
+                VisualFeatures.OBJECTS,
+                VisualFeatures.PEOPLE],
+        )
+
+    except HttpResponseError as e:
+        print(f"Status code: {e.status_code}")
+        print(f"Reason: {e.reason}")
+        print(f"Message: {e.error.message}")
+
+    # Display analysis results
+    # Get image captions
+    if result.caption is not None:
+        print("\nCaption:")
+        print(" Caption: '{}' (confidence: {:.2f}%)".format(result.caption.text, result.caption.confidence * 100))
+
+    # Get image dense captions
+    if result.dense_captions is not None:
+        print("\nDense Captions:")
+        for caption in result.dense_captions.list:
+            print(" Caption: '{}' (confidence: {:.2f}%)".format(caption.text, caption.confidence * 100))
+
+    # Get image tags
+    if result.tags is not None:
+        print("\nTags:")
+        for tag in result.tags.list:
+            print(" Tag: '{}' (confidence: {:.2f}%)".format(tag.name, tag.confidence * 100))
+
+    # Get objects in the image
+    if result.objects is not None:
+        print("\nObjects in image:")
+
+        # Prepare image for drawing
+        image = Image.open(image_filename)
+        fig = plt.figure(figsize=(image.width/100, image.height/100))
+        plt.axis('off')
+        draw = ImageDraw.Draw(image)
+        color = 'cyan'
+
+        for detected_object in result.objects.list:
+            # Print object name
+            print(" {} (confidence: {:.2f}%)".format(detected_object.tags[0].name, detected_object.tags[0].confidence * 100))
+            
+            # Draw object bounding box
+            r = detected_object.bounding_box
+            bounding_box = ((r.x, r.y), (r.x + r.width, r.y + r.height)) 
+            draw.rectangle(bounding_box, outline=color, width=3)
+            plt.annotate(detected_object.tags[0].name,(r.x, r.y), backgroundcolor=color)
+
+        # Save annotated image
+        plt.imshow(image)
+        plt.tight_layout(pad=0)
+        outputfile = 'objects.jpg'
+        fig.savefig(outputfile)
+        print('  Results saved in', outputfile)
+
+    # Get people in the image
+    # Get people in the image
+    if result.people is not None:
+        print("\nPeople in image:")
+
+        # Prepare image for drawing
+        image = Image.open(image_filename)
+        fig = plt.figure(figsize=(image.width/100, image.height/100))
+        plt.axis('off')
+        draw = ImageDraw.Draw(image)
+        color = 'cyan'
+
+        for detected_people in result.people.list:
+            # Draw object bounding box
+            r = detected_people.bounding_box
+            bounding_box = ((r.x, r.y), (r.x + r.width, r.y + r.height))
+            draw.rectangle(bounding_box, outline=color, width=3)
+
+            # Return the confidence of the person detected
+            #print(" {} (confidence: {:.2f}%)".format(detected_people.bounding_box, detected_people.confidence * 100))
+            
+        # Save annotated image
+        plt.imshow(image)
+        plt.tight_layout(pad=0)
+        outputfile = 'people.jpg'
+        fig.savefig(outputfile)
+        print('  Results saved in', outputfile)
 
 
-    # Get image analysis
-
-
-
-def BackgroundForeground(image_file, cv_client):
-    print('\n')
+def BackgroundForeground(endpoint, key):
+    print('\nRemoving background from image...')
+    
+    # Define the API version and mode
+    api_version = "2023-02-01-preview"
+    mode="backgroundRemoval" # Can be "foregroundMatting" or "backgroundRemoval"
     
     # Remove the background from the image or generate a foreground matte
+    url = "{}computervision/imageanalysis:segment?api-version={}&mode={}".format(endpoint, api_version, mode)
 
+    headers= {
+        "Ocp-Apim-Subscription-Key": key, 
+        "Content-Type": "application/json" 
+    }
+    # You can change the url to use other images in the images folder,
+    # such as "building.jpg" or "person.jpg" to see different results.
+    body = {
+        "url": "https://github.com/MicrosoftLearning/mslearn-ai-vision/blob/main/Labfiles/01-analyze-images/Python/image-analysis/images/street.jpg?raw=true",
+    }
+        
+    response = requests.post(url, headers=headers, json=body)
+
+    image=response.content
+    with open("backgroundForeground.png", "wb") as file:
+        file.write(image)
+    print('  Results saved in backgroundForeground.png \n')
 
 
 if __name__ == "__main__":
