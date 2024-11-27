e63d87068217a86cedbc97c513e9c794d6b64ffc
Siddharth Saklecha <siddharth2408@gmail.com>
2024-11-27 02:37:32 -0800

    Python: Add code for using vision AI lib

--- Labfiles/01-analyze-images/Python/image-analysis/image-analysis.py
+++ Labfiles/01-analyze-images/Python/image-analysis/image-analysis.py
@@ -7,7 +7,10 @@ from azure.core.exceptions import HttpResponseError
 import requests
 
 # Import namespaces
-
+# import namespaces
+from azure.ai.vision.imageanalysis import ImageAnalysisClient
+from azure.ai.vision.imageanalysis.models import VisualFeatures
+from azure.core.credentials import AzureKeyCredential
 
 def main():
     global cv_client
@@ -27,7 +30,11 @@ def main():
             image_data = f.read()
 
         # Authenticate Azure AI Vision client
-
+        # Authenticate Azure AI Vision client
+        cv_client = ImageAnalysisClient(
+            endpoint=ai_endpoint,
+            credential=AzureKeyCredential(ai_key)
+        )
         
         # Analyze image
         AnalyzeImage(image_file, image_data, cv_client)
@@ -44,7 +51,16 @@ def AnalyzeImage(image_filename, image_data, cv_client):
 
     try:
         # Get result with specified features to be retrieved
-        
+        # Get result with specified features to be retrieved
+        result = cv_client.analyze(
+            image_data=image_data,
+            visual_features=[
+                VisualFeatures.CAPTION,
+                VisualFeatures.DENSE_CAPTIONS,
+                VisualFeatures.TAGS,
+                VisualFeatures.OBJECTS,
+                VisualFeatures.PEOPLE],
+        )
 
     except HttpResponseError as e:
         print(f"Status code: {e.status_code}")
@@ -52,14 +68,113 @@ def AnalyzeImage(image_filename, image_data, cv_client):
         print(f"Message: {e.error.message}")
 
     # Display analysis results
+    # Display analysis results
+    # Get image captions
+    if result.caption is not None:
+        print("\nCaption:")
+        print(" Caption: '{}' (confidence: {:.2f}%)".format(result.caption.text, result.caption.confidence * 100))
+
+    # Get image dense captions
+    if result.dense_captions is not None:
+        print("\nDense Captions:")
+        for caption in result.dense_captions.list:
+            print(" Caption: '{}' (confidence: {:.2f}%)".format(caption.text, caption.confidence * 100))
+
+    # Get image tags
+    # Get image tags
+    if result.tags is not None:
+        print("\nTags:")
+        for tag in result.tags.list:
+            print(" Tag: '{}' (confidence: {:.2f}%)".format(tag.name, tag.confidence * 100))
+
+
+    # Get objects in the image
+    # Get objects in the image
+    if result.objects is not None:
+        print("\nObjects in image:")
+
+        # Prepare image for drawing
+        image = Image.open(image_filename)
+        fig = plt.figure(figsize=(image.width/100, image.height/100))
+        plt.axis('off')
+        draw = ImageDraw.Draw(image)
+        color = 'cyan'
+
+        for detected_object in result.objects.list:
+            # Print object name
+            print(" {} (confidence: {:.2f}%)".format(detected_object.tags[0].name, detected_object.tags[0].confidence * 100))
+
+            # Draw object bounding box
+            r = detected_object.bounding_box
+            bounding_box = ((r.x, r.y), (r.x + r.width, r.y + r.height)) 
+            draw.rectangle(bounding_box, outline=color, width=3)
+            plt.annotate(detected_object.tags[0].name,(r.x, r.y), backgroundcolor=color)
+
+        # Save annotated image
+        plt.imshow(image)
+        plt.tight_layout(pad=0)
+        outputfile = 'objects.jpg'
+        fig.savefig(outputfile)
+        print('  Results saved in', outputfile)
+
+
+    # Get people in the image
+    # Get people in the image
+    if result.people is not None:
+        print("\nPeople in image:")
+
+        # Prepare image for drawing
+        image = Image.open(image_filename)
+        fig = plt.figure(figsize=(image.width/100, image.height/100))
+        plt.axis('off')
+        draw = ImageDraw.Draw(image)
+        color = 'cyan'
+
+        for detected_people in result.people.list:
+            # Draw object bounding box
+            r = detected_people.bounding_box
+            bounding_box = ((r.x, r.y), (r.x + r.width, r.y + r.height))
+            draw.rectangle(bounding_box, outline=color, width=3)
+
+            # Return the confidence of the person detected
+            print(" {} (confidence: {:.2f}%)".format(detected_people.bounding_box, detected_people.confidence * 100))
+
+        # Save annotated image
+        plt.imshow(image)
+        plt.tight_layout(pad=0)
+        outputfile = 'people.jpg'
+        fig.savefig(outputfile)
+        print('  Results saved in', outputfile)
     
 
 def BackgroundForeground(endpoint, key, image_file):
     # Define the API version and mode
     api_version = "2023-02-01-preview"
-    mode="backgroundRemoval" # Can be "foregroundMatting" or "backgroundRemoval"
+    mode="foregroundMatting" # Can be "foregroundMatting" or "backgroundRemoval"
     
     # Remove the background from the image or generate a foreground matte
+    # Remove the background from the image or generate a foreground matte
+    print('\nRemoving background from image...')
+
+    url = "{}computervision/imageanalysis:segment?api-version={}&mode={}".format(endpoint, api_version, mode)
+
+    headers= {
+        "Ocp-Apim-Subscription-Key": key, 
+        "Content-Type": "application/json" 
+    }
+
+    image_url="https://github.com/MicrosoftLearning/mslearn-ai-vision/blob/main/Labfiles/01-analyze-images/Python/image-analysis/{}?raw=true".format(image_file)  
+
+    body = {
+        "url": image_url,
+    }
+
+    response = requests.post(url, headers=headers, json=body)
+
+    image=response.content
+    with open("background.png", "wb") as file:
+        file.write(image)
+    print('  Results saved in background.png \n')
     
 
 
