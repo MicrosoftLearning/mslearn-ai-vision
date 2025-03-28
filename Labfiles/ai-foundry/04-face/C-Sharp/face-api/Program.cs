941a52c3328f7760295199f53446e943963e880a
Alison Felix <felix.alison@outlook.com>
2025-03-28 17:59:54 +0000

    Update Program.cs

--- Labfiles/ai-foundry/04-face/C-Sharp/face-api/Program.cs
+++ Labfiles/ai-foundry/04-face/C-Sharp/face-api/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using System.Drawing;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
